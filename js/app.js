d47b2a706f1fdd1e3388a88076fdc97ae81d357d
Rama Herbin <ramacharles.h@gmail.com>
2022-10-23 16:12:58 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three';
+import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'meshline';
+
+
+
+export default class Application {
+
+    constructor(_params) {
+        this.$canvas = _params.$canvas;
+
+        
+    }
+}
---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+
+
+import Application from './js/app.js';
+
+window.application = new Application({
+    $canvas: document.querySelector('.js-canvas'),
+});
